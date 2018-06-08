75f4315162ad1a436cf1d42014c9a5f3ae9f5336
Ciske H <ciske_harsema_2@hotmail.com>
2018-06-09 00:12:32 +0200

    Added page to POST bookings to

---
+++ src/rnd/BookServlet.java
@@ -0,0 +1,26 @@
+package rnd;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class BookServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String content = StreamUtils.toString(req.getInputStream());
+
+        log("Got book post: " + content);
+
+        int bookingID = ThreadLocalRandom.current().nextInt();
+        resp.setContentType("text/plain");
+
+        try (PrintWriter writer = resp.getWriter()) {
+            writer.print(Integer.toString(bookingID));
+        }
+    }
+}
